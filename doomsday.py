050126149fbc7f29178058ac392cda864e6d977b
Kabilshr <kabilshrestha06@gmail.com>
2022-10-10 12:33:53 +0545

    Improvements and added leap year function

--- doomsday.py
+++ doomsday.py
@@ -1,20 +1,34 @@
-
-def limit(date, month):
+#date limit function
+def date_limit(date):
     if date > 31:
         print("There can't be more than 31 days in a months.")
     else:
         return date
+
+#month limit function
+def month_limit(month):
     if month > 12:
         print("There can't be more than 12 months.")
     else:
         return month
 
-
+#tells if it is a leap year.
+def lp_year(year):
+    year = int(year)
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                lp_year = True
+            else:
+                lp_year = False
+        else:
+            lp_year = True
+    else:
+        lp_year = False
 
 print("Welcome to DOOMSDAY! This is a program that tells you day of any date.\nGive it a try!")
 while(True):
     date = eval(input("Enter date: "))
-    print(type(date))
     if type(date) != int:
         print("Please type an integer number in date!")
     month = eval(input("Enter month: "))
@@ -23,5 +37,14 @@ while(True):
     year = eval(input("Enter year: "))
     if type(year) != int:
         print("Please type an integer  in year!")
+
     #limit check
-    limit(date, month)
+    date_limit(date) 
+    month_limit(month)
+
+    #check leap year
+    lp_year(year)
+
+    print("result")
+
+    
