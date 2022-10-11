fd5b02dcc44f3af333afc2d817473a82806ec0fe
Kabil Shrestha <96244118+Kabilshr@users.noreply.github.com>
2022-10-11 20:54:35 +0545

    working test loop

---
+++ loop_test.py
@@ -0,0 +1,11 @@
+#while loop 
+x = 0
+while True:
+    x+=1   
+    if x >1:
+        data = input("do you want to try again?y/n  ")
+        if data == "n":
+            break
+    else:
+        print("looping")
+        print(x)
