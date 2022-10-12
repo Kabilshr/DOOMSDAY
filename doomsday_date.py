523058c3505215862c07a9a56df1f7877bda5b45
Ki <106600652+Ki-69@users.noreply.github.com>
2022-10-12 15:30:52 +0545

    updated. more pythonic and is functional
    ...

--- doomsday_date.py
+++ doomsday_date.py
@@ -5,18 +5,8 @@
 is_lp_year = True
 
 date = int(input('Enter date:'))
-month = int(input('Enter the month:'))
-doomsday = int(input('enter doomsday:'))
-def closest(llst, month):
-	return llst[min(range(len(llst)), key = lambda i: abs(llst[i]-month))]
-llst = [1,2,3,4,5,6,7,8,9,10,11,12]
-doom_month = int(closest(llst, month))
-k = int((llst.index(doom_month)))
-if (month - doom_month) < 0:
-    doom_month = llst[(k - 1)]
-else:
-    doom_month = int(closest(llst, month))
-print(doom_month)
+doom_month = int(input('Enter the month:'))
+doomsday = 1
 
 if doom_month == 1:
     if is_lp_year == True:
@@ -48,31 +38,13 @@ elif doom_month == 11:
     dday = date - 7
 elif doom_month == 12:
     dday = date - 12
-
 if dday > 0:
-    def clos(lst, dday):
-	    return lst[min(range(len(lst)), key = lambda i: abs(lst[i]-dday))]
-    
-    
-    lst = [0, 7, 14, 21, 27, 35, 42, 49, 54, 63, 70, 77, 84, 91, 98]
-    k=int((clos(lst, dday)))
-    x=int((lst.index(k)))
-    if (dday - k) < 0:
-        k = lst[(x - 1)]
-        print(k)
-    else:
-        k = int(clos(lst, dday))
-    day = dday - k
+    day = dday % 7
     day_date = doomsday + day
 elif dday < 0:
-    def clos(lst, dday):
-	    return lst[min(range(len(lst)), key = lambda i: abs(lst[i]-dday))]
-    
-    lst = [-35,-27,-21,-14,-7, 0]
-    k=int((clos(lst, dday)))
-    x=int((lst.index(k)))
-    day = abs(dday - k)
+    day = abs(dday % -7)
     day_date = doomsday - day
+    
 elif dday == 0:
     day_date = doomsday
 
